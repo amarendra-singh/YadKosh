6e0e8f6e8b06fc8b30fe77720519415bb24c46b7
amarendra-singh <imamarendra.singh@gmail.com>
2022-11-15 07:36:24 +0000

    Database creation, Schemas,ORM, Models

--- Basic/main.py
+++ Basic/main.py
@@ -1,13 +1,17 @@
-
 from fastapi import FastAPI
-from router import BlogGet,BlogPost
+from router import BlogGet,BlogPost,user
+
+from db import models
+from db.database import engine 
 
 app = FastAPI()
 app.include_router(BlogGet.router)
 app.include_router(BlogPost.router)
+app.include_router(user.router)
 
 @app.get('/')
 def index():
     return {"message":"Hello world"}
 
-
+# Ceate Database
+models.Base.metadata.create_all(engine)
