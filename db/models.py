740db126f81720a9503989b318a31d892e49d718
Amarendra Singh <imamarendra.singh@gmail.com>
2023-02-03 18:09:55 +0530

    articles

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import ForeignKey
 from db.database import Base
 from sqlalchemy import Column
 
@@ -8,3 +9,13 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship('DbArticles', back_populates = 'user')
+
+class DbArticles(Base):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("DbUser", back_populates = 'items')
